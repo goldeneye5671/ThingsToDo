8456aa42b91bf8eaa32c38831fc7161b0876d09f
Anthony Seefried <59634572+goldeneye5671@users.noreply.github.com>
2023-05-23 14:06:23 -0700

    Feature/things to do get businesses (#19)
    * [#16 & #17] initialized the routes;
    
    * [#16] finished the route to get all businesses
    
    * [#17] finished the route to get one business;
    
    * [#16] fixed a typo;

--- backend/routes/businesses.js
+++ backend/routes/businesses.js
@@ -3,6 +3,28 @@ const expressAsyncHandler = require("express-async-handler")
 const db = require("../db/models")
 const {Op} = require("sequelize")
 
+router.get("/", expressAsyncHandler(async (req, res, next) => {
+    try {
+        const allBusinesses = await db.Business.findAll()
+        res.json(allBusinesses)
+    } catch (e) {
+        next(e)
+    }
+}))
+
+router.get("/:businessId", expressAsyncHandler(async (req, res, next) => {
+    try {
+        const businessByPk = await db.Business.findByPk(parseInt(req.params.businessId));
+        if (businessByPk) {
+            res.json(businessByPk)
+        } else {
+            res.status(500).json({message: "Resource could not be found"})
+        }
+    } catch (e) {
+
+    }
+}))
+
 
 router.post("/", expressAsyncHandler(async (req, res, next) => {
     try {
