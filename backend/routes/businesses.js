b3aa0657837cfd729f6fc096d8c2218a6bf49d72
goldeneye5671 <choiceaas@gmail.com>
2023-05-23 13:45:24 -0700

    [#1] Created a route that can create a business. Added the business routes to the app.js files to be used by aPI;

---
+++ backend/routes/businesses.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const expressAsyncHandler = require("express-async-handler")
+const db = require("../db/models")
+const {Op} = require("sequelize")
+
+
+router.post("/", expressAsyncHandler(async (req, res, next) => {
+    try {
+        const {
+            name,
+            primaryPhoto,
+            address,
+            city,
+            stateProvince,
+            country,
+            zipcode
+        } = req.body;
+
+        if (name && city && stateProvince && country && zipcode) {
+            const newBusiness = await db.Business.create({
+                name, primaryPhoto ,address, city, stateProvince, country, zipcode
+            })
+
+            res.json(newBusiness)
+        } else {
+            throw new Error("Not all required info was provided for a business to be created")
+        }
+    } catch (e) {
+        next(e)
+    }
+}))
+
+module.exports = router
