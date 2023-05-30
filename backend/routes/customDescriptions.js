65f063e246a2b0d3297642b9dcac69d71c1d7000
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2023-05-30 12:36:39 -0700

    [#23 #24 #27] CRUD Functionality added for customDesc

---
+++ backend/routes/customDescriptions.js
@@ -0,0 +1,94 @@
+const router = require("express").Router();
+const expressAsyncHandler = require("express-async-handler")
+const db = require("../db/models");
+const { el } = require("@faker-js/faker");
+
+
+router.get("/", expressAsyncHandler(async (req, res, next) => {
+    try {
+        res.json(await db.CustomDescription.findAll());
+    } catch (e) {
+        next(e)
+    }
+}));
+
+router.post("/",expressAsyncHandler(async (req, res, next) => {
+    try {
+        const {
+            userId,
+            thingToDoId,
+            headline,
+            description,
+        } = req.body
+
+        if (userId && thingToDoId && headline && description) {
+            const newDesc = await db.CustomDescription.create({
+                userId,
+                thingToDoId,
+                headline,
+                description
+            })
+    
+            res.json(newDesc)
+        } else {
+            throw new Error("Not all required info was provided for a description to be created")
+        }
+    } catch (e) {
+        next(e)
+    }
+}))
+
+router.get("/:customDescriptionId", expressAsyncHandler(async (req, res, next) => {
+    try {
+        const desc = await db.CustomDescription.findByPk(parseInt(req.params.customDescriptionId))
+        if (desc) {
+            res.json(desc)
+        } else {
+            throw new Error("Could not find resource")
+        }
+    }catch (e) {
+        next(e)
+    }
+}))
+
+
+router.patch("/:customDescriptionId", expressAsyncHandler(async (req, res, next) => {
+    try {
+        const desc = await db.CustomDescription.findByPk(parseInt(req.params.customDescriptionId))
+        if (desc) { 
+            const headline = req.body.headline ?? desc.headline;
+            const description = req.body.description ?? desc.description
+
+            await desc.update({
+                headline,
+                description
+            });
+
+            res.json(await db.CustomDescription.findByPk(parseInt(req.params.customDescriptionId)))
+        } else {
+            throw new Error("Resource not found")
+        }
+    } catch (e) {
+        next(e)
+    }
+}));
+
+router.delete("/:customDescriptionId", expressAsyncHandler(async (req, res, next) => {
+    try {
+        const desc = await db.CustomDescription.findByPk(parseInt(req.params.customDescriptionId));
+
+        if (desc) {
+            await desc.destroy()
+            res.json({
+                message: "Resource Deleted"
+            })
+        } else {
+            throw new Error("Resource not found")
+        }
+    } catch (e) {
+        next(e)
+    }
+}));
+
+
+module.exports = router;
