3da06a6e402ff60a6dd9cc6faeeddfbce0212c63
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2023-08-08 12:13:31 -0700

    tutorial for react-router-dom

--- bliss/src/main.tsx
+++ bliss/src/main.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter({
-  path: "/",
-  element 
-})
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root/>,
+    errorElement: <ErrorPage />
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router}></RouterProvider>
+	</React.StrictMode>
+);
