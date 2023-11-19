493d7daf946dc1352d79c4bdf590366e4b862b40
Anthony Seefried <59634572+goldeneye5671@users.noreply.github.com>
2023-11-19 11:08:24 -0600

    Hotfix2 (#75)
    * changed the CORS settings to allow from onrender
    
    * changed origin
    
    * Changed some things so vite knows what env it's in
    so it loads to the right spot

--- bliss-app/src/utils/axiosInstance.js
+++ bliss-app/src/utils/axiosInstance.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import process from "process";
 
+const env = process.env.NODE_ENV || "development";
+
+console.log("env")
+
+//change this when you deploy you dumb ass
 export default axios.create({
     withCredentials: true,
-    baseURL: "http://127.0.0.1:5000"
+    baseURL: env === "production" ? "/" : "http://127.0.0.1:5000"
 })
