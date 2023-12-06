46a83ecf7554dc396c4862934ab177911ba56edf
Anthony Seefried <59634572+goldeneye5671@users.noreply.github.com>
2023-12-06 09:15:24 -0800

    List page (#96)
    * Fixed an issue
    Incorrect data was being sent back, altering the state incorrectly
    
    * Added search to bliss and list
    
    * bug fix
    
    Fixed an issue where list creation would fail when adding tags. This
    was due to it adding list tags to a list that hasn't been created yet.
    
    * Updated bliss page layout
    bliss names are on the side and the content is on the other side.
    in the future, mobile will function similarly to the way it did before
    added infinite scrolling for the bliss name col
    updated some things to use rtkq.
    more components will be migrated over to rtkq
    
    * removed unused comments
    
    * fixed an issue with getting data from the right place
    
    * updated packages and fixed issues
    
    * changes
    
    * attempting a fix

--- bliss-app/src/utils/axiosInstance.js
+++ bliss-app/src/utils/axiosInstance.js
@@ -5,3 +5,5 @@ export default axios.create({
     withCredentials: true,
     baseURL: process.env.NODE_ENV === "production" ? "/" :"http://localhost:5000"
 })
+
+export const isProduction = process.env.NODE_ENV === "production" ? true : false;
