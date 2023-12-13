e6a6ae04d019e5635f6dfa0d78eabe71b8c3f884
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2023-12-12 19:44:53 -0800

    added in router

--- bliss/src/App.jsx
+++ bliss/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    
-    </>
-  )
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sign-up" element={<SignUp />}/>
+				<Route path="/sign-in" element={<SignIn />}/>
+				<Route path="/bliss" element={<BlissPage />}>
+					<Route path="/bliss/:id" element={<IndividualBliss/>}/>
+				</Route>
+				<Route path="/businesses" element={<BusinessPage />}></Route>
+				<Route path="/lists" element={<ListPage />}>
+					<Route path={"/lists/:id"} element={<IndividualList />} />
+				</Route>
+				<Route path="/users/:id" element={<UserPage />}/>
+			</Routes>
+		</>
+	);
 }
 
 export default App
